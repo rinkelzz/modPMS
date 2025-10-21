d0ce86ca579986da391869535b376d3e41ce5bea
rinkelzz <55958954+rinkelzz@users.noreply.github.com>
2025-10-21 08:31:02 +0200

    Add configurable reservation status colors

---
+++ config/app.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'name' => 'modPMS',
+    'version' => '1.6.1',
+    'repository' => [
+        'url' => 'https://github.com/rinkelzz/modpms',
+        'branch' => 'main',
+    ],
+    'modules' => [
+        'dashboard' => [
+            'title' => 'Dashboard',
+            'description' => 'Übersicht mit Kalender, Anzeigeoptionen und Reservierungsstatus.'
+        ],
+        'reservations' => [
+            'title' => 'Reservierungen',
+            'description' => 'Aufenthalte verwalten und Historien nachvollziehen.'
+        ],
+        'guests' => [
+            'title' => 'Gästeverwaltung',
+            'description' => 'Stammdaten für Gäste und Firmen inkl. Meldeschein-relevanter Felder.'
+        ],
+        'rooms' => [
+            'title' => 'Zimmerkategorien',
+            'description' => 'Verwaltung von Kategorien, Kapazitäten und Status.'
+        ],
+        'updates' => [
+            'title' => 'Systemupdates',
+            'description' => 'Version anzeigen und Updates aus GitHub abrufen.'
+        ],
+        'users' => [
+            'title' => 'Benutzerverwaltung',
+            'description' => 'Zugangsdaten verwalten und Rollen steuern.'
+        ],
+        'settings' => [
+            'title' => 'Einstellungen',
+            'description' => 'Statusfarben und Kalenderdarstellung anpassen.'
+        ],
+    ],
+];
